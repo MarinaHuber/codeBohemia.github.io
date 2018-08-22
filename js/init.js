1d4fa7255a60cf9b4c244156163f8240181bef16
Marina Huber <huber.marina2gmail.com>
2018-08-22 15:50:35 +0200

    directive template

--- js/init.js
+++ js/init.js
@@ -1,55 +1,35 @@
 /*
-	Read Only by HTML5 UP
+	Directive by HTML5 UP
 	html5up.net | @n33co
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+
 (function($) {
 
 	skel.init({
 		reset: 'full',
 		breakpoints: {
-			global: { href: 'css/style.css', containers: '45em', grid: { gutters: ['2em', 0] } },
-			xlarge: { media: '(max-width: 1680px)', href: 'css/style-xlarge.css' },
-			large: { media: '(max-width: 1280px)', href: 'css/style-large.css', containers: '42em', grid: { gutters: ['1.5em', 0] }, viewport: { scalable: false } },
-			medium: { media: '(max-width: 1024px)', href: 'css/style-medium.css', containers: '85%!' },
-			small: { media: '(max-width: 736px)', href: 'css/style-small.css', containers: '90%!', grid: { gutters: ['1.25em', 0] } },
-			xsmall: { media: '(max-width: 480px)', href: 'css/style-xsmall.css' }
-		},
-		plugins: {
-			layers: {
-				config: {
-					mode: 'transform'
-				},
-				titleBar: {
-					breakpoints: 'medium',
-					width: '100%',
-					height: 44,
-					position: 'top-left',
-					side: 'top',
-					html: '<span class="toggle" data-action="toggleLayer" data-args="sidePanel"></span><span class="title" data-action="copyText" data-args="logo"></span>'
-				},
-				sidePanel: {
-					breakpoints: 'medium',
-					hidden: true,
-					width: { small: 275, medium: '20em' },
-					height: '100%',
-					animation: 'pushX',
-					position: 'top-right',
-					side: 'right',
-					orientation: 'vertical',
-					clickToHide: true,
-					html: '<div data-action="moveElement" data-args="header"></div>'
-				}
-			}
+			global:		{ range: '*', href: 'css/style.css', containers: '51em', grid: { gutters: 30 } },
+			wide:		{ range: '-1680', href: 'css/style-wide.css' },
+			normal:		{ range: '-1280', href: 'css/style-normal.css', containers: '48em' },
+			narrow:		{ range: '-980', href: 'css/style-narrow.css', containers: '95%', grid: { gutters: 30 } },
+			narrower:	{ range: '-840', href: 'css/style-narrower.css', containers: '95%!', grid: { gutters: 20 } },
+			mobile:		{ range: '-736', href: 'css/style-mobile.css', containers: '90%!', grid: { gutters: 20 }, viewport: { scalable: false } },
+			mobilep:	{ range: '-480', href: 'css/style-mobilep.css', containers: '100%!' }
 		}
 	});
 
 	$(function() {
 
-		var $body = $('body'),
-			$header = $('#header'),
-			$nav = $('#nav'), $nav_a = $nav.find('a'),
-			$wrapper = $('#wrapper');
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				$body.removeClass('is-loading');
+			});
 
 		// Forms (IE<10).
 			var $form = $('form');
@@ -67,53 +47,6 @@
 				}
 
 			}
-
-		// Header.
-			var ids = [];
-
-			// Set up nav items.
-				$nav_a
-					.scrolly()
-					.on('click', function(event) {
-
-						var $this = $(this),
-							href = $this.attr('href');
-
-						// Not an internal link? Bail.
-							if (href.charAt(0) != '#')
-								return;
-
-						// Prevent default behavior.
-							event.preventDefault();
-
-						// Remove active class from all links and mark them as locked (so scrollzer leaves them alone).
-							$nav_a
-								.removeClass('active')
-								.addClass('scrollzer-locked');
-
-						// Set active class on this link.
-							$this.addClass('active');
-
-					})
-					.each(function() {
-
-						var $this = $(this),
-							href = $this.attr('href'),
-							id;
-
-						// Not an internal link? Bail.
-							if (href.charAt(0) != '#')
-								return;
-
-						// Add to scrollzer ID list.
-							id = href.substring(1);
-							$this.attr('id', id + '-link');
-							ids.push(id);
-
-					});
-
-			// Initialize scrollzer.
-				$.scrollzer(ids, { pad: 300, lastHack: true });
 
 	});
 
